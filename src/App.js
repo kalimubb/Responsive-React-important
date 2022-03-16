39e0a6640a42b8cbd592b4e957fe33c09e5d3af3
Kalimubb <mahjourkarim@gmail.com>
2022-03-16 12:24:22 +0100

    first drop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GlobalStyle } from "./globalStyles";
+import { Switch,BrowserRouter,Route,Routes } from 'react-router-dom'
+
+//Pages
+import Homepage from "./pages/Homepage";
+import Pricingpage from "./pages/Pricingpage";
+import Signuppage from "./pages/Signuppage";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <GlobalStyle />
+    <Routes>
+      
+      <Route exact path='/' element={<Homepage />} />
+      <Route exact path='/pricing' element={<Pricingpage />} />
+      <Route exact path='/signup' element={<Signuppage />} />
+  
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
